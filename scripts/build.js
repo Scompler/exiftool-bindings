21d5d2b4153add177f9677777dbfe04a7ef21068
Oleg German <oleg.german@gmail.com>
2021-09-15 20:38:36 +0300

    Skip struct childs, and duplicates

--- scripts/build.js
+++ scripts/build.js
@@ -10,7 +10,7 @@ const exiftool = require('@mcmics/dist-exiftool');
 const whiteList = {
   'xmp': {
     'types': ['real', 'string', 'integer', 'date', 'boolean', 'rational'],
-    'tables': ['XMP::Device', 'XMP::cc', 'XMP::aux', 'XMP::dc', 'XMP::exif', 'XMP::exifEX', 'XMP::ics', 'XMP::iptcCore', 'XMP::iptcExt', 'XMP::pdf', 'XMP::photoshop', 'XMP::pdf', 'XMP::tiff', 'XMP::xmp', 'XMP::xmpRights']
+    'tables': ['XMP::Device', 'XMP::cc', 'XMP::exif', 'XMP::exifEX', 'XMP::ics', 'XMP::iptcCore', 'XMP::iptcExt', 'XMP::xmp', 'XMP::xmpRights']
   },
   'exif': {
     'types': ['string', 'int16u', 'rational64u', 'int16s', 'rational64s'],
@@ -26,6 +26,7 @@ const langs = ['en', 'de']
 let langDump = {};
 
 Object.keys(whiteList).forEach((sectionName) => {
+  let blacklistPrefixes = [];
   execFile(exiftool, ['-listx', `-${sectionName}:all`], (error, stdout, stderr) => {
     if (error) { console.log('exiftool error', stderr) }
     parseString(stdout, function (err, result) {
@@ -39,9 +40,17 @@ Object.keys(whiteList).forEach((sectionName) => {
         return previous.concat(current);
       });
 
-      array = array.filter(tag =>  {
-        if (whiteList[sectionName].types.includes(tag.$.type)) return true
-        console.log(`Skipping ${sectionName} field ${tag.$.name} with type ${tag.$.type}`);
+      array = array.filter(tag => {
+        if (hasDuplicate(tag, array) && tag.$.type === 'string') {
+          console.log(`Skipping ${sectionName} field ${tag.$.name} with type ${tag.$.type} because it has duplicate`);
+        } else if (whiteList[sectionName].types.includes(tag.$.type)) {
+          return true
+        } else if (blacklistPrefixes.some(prefix => tag.$.name.startsWith(prefix))) {
+          console.log(`Skipping ${sectionName} field ${tag.$.name} with type ${tag.$.type} as a child of struct`);
+        } else {
+          if (tag.$.type === 'struct') blacklistPrefixes.push(tag.$.name)
+          console.log(`Skipping ${sectionName} field ${tag.$.name} with type ${tag.$.type}`);
+        }
         return false;
       });
 
@@ -67,6 +76,11 @@ const extractTag = (tag) => {
   if (tag.values) output['values'] = tag.values[0].key.map(option => option.$.id)
 
   return output
+}
+
+const hasDuplicate = (tag, array) => {
+  const duplicates = array.filter(currentTag => (currentTag.$.name === tag.$.name))
+  return duplicates.length > 1 && _.last(duplicates).$.id === tag.$.id
 }
 
 const extractTranslation = (tag, sectionName) => {
