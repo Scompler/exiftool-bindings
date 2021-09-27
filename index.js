41d58a7e91d0eaf3af8a1549f55cf624bc6bb9d0
Oleg German <oleg.german@gmail.com>
2021-09-27 05:02:41 +0300

    Mime types

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import exif from './json/exif.json';
 import iptc from './json/iptc.json';
 import xmp from './json/xmp.json';
 
+import readMimeTypes from './json/read-mime-types.json';
+import writeMimeTypes from './json/write-mime-types.json';
+
 export default { exif, iptc, xmp };
