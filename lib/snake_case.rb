350a55a4eb0a73288910d571ae655880e10f509c
Oleg German <oleg.german@gmail.com>
2021-10-22 19:52:15 +0300

    Update snake case

--- lib/snake_case.rb
+++ lib/snake_case.rb
@@ -6,10 +6,12 @@ module ExiftoolBindings
       def snake_case
         # Lodash like underscore
         # Converts 'FocalLengthIn35mmFormat' to focal_length_in_35_mm_format
+        # Or XMP-xmp:ModifyDate to xmp_xmp_modify_date
         # Unlike underscore (focal_length_in35mm_format)
         word = underscore
         word.gsub!(/([a-z])([\d])/, '\1_\2')
         word.gsub!(/([\d])([a-z])/, '\1_\2')
+        word.gsub!(':', '_')
         word
       end
     end
